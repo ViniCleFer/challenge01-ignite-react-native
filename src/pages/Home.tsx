c1240591cd31d1b1616294a99cea35b5a3428986
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2021-08-19 16:05:36 -0300

    feat: addTask, taskDone and removeTask

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,32 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: tasks?.length + 1,
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks([...tasks, task])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const tasksList = tasks?.map((item: Task) => {
+      if (item.id === id) {
+        return ({
+          ...item,
+          done: !item?.done
+        })
+      }
+    });
+
+    setTasks(tasksList as Task[]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const tasksList = tasks?.filter((item: Task) => item?.id !== id);
+
+    setTasks(tasksList as Task[]);
   }
 
   return (
